584bdabee78767ef8a131e19909d25307786a06b
goelhardik <hardik.iitr@gmail.com>
2016-01-06 18:10:59 -0600

    Find the missing number in the array of 0...n numbers

---
+++ leetcode/missing_number/sol.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        sum = n * (n + 1) // 2
+        for i in range(n):
+            sum -= nums[i]
+            
+        return sum
